585470a0ab90861a3b2cd411d2a325b08ea6bab1
Gergo <ferenczi.gergo.87@gmail.com>
2021-06-22 21:30:31 +0200

    half done

---
+++ src/main/java/com/codecool/shop/controller/login/LoginController.java
@@ -0,0 +1,25 @@
+package com.codecool.shop.controller.login;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+@WebServlet(urlPatterns={"/login"})
+public class LoginController extends HttpServlet {
+
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String username = req.getParameter("name");
+        String password = req.getParameter("password");
+
+        if(username.equals("alma") && password.equals("alma")) {
+            HttpSession session = req.getSession();
+            session.setAttribute("name", username);
+        }
+
+        resp.sendRedirect(req.getHeader("referer"));
+    }
+}
